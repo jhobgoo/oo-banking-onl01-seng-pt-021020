3c020cb16d8cd0389c319b69727e35430286f497
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 05:10:35 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,29 +14,29 @@ class Transfer
     sender.valid? && receiver.valid?
   end
   
+  def reject_transfer
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+  
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
+    if valid? && sender.balance > amount && @status == "pending"
       receiver.balance += amount
-      self.status = "complete"
+      sender.balance -= amount
+      @status = "complete"
     else
       reject_transfer
     end
   end
   
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
+    if valid? && receiver.balance > amount && @status == "complete"
       sender.balance += amount
       receiver.balance -= amount
-      self.status = "reversed"
+      @status = "reversed"
     else
       reject_transfer
     end
-  end
-  
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
   end
 
 end
